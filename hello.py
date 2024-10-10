b57bd2b3a4a3efe35484439e5ab2e0176f63fd1d
C22793881 Alwalid Bouderbala <C22793881@mytudublin.ie>
2024-10-10 15:58:16 +0100

    Added /about route and modified / route to link to it

--- hello.py
+++ hello.py
@@ -1,11 +1,17 @@
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<p>Hello, World!</p>'
+    return '<p>Hello, World!</p><br><a href="/about">About</a>'
+
+@app.route('/about')
+def about():
+    return '<p>This is a Flask web app running in a Linux VM.</p>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
 
